18cf3a709cce336c132ebc9c1273d826a5858aad
ozo7 <send2olaf@yahoo.de>
2020-02-21 14:06:34 +0100

    xml and button functions

--- openacademy/models/zzhelper.py
+++ openacademy/models/zzhelper.py
@@ -2,23 +2,35 @@
 from odoo import api, fields, models
 from random import randint
 
-class zzHelper(models.Model):
+class zzHelper(models.TransientModel):
     # Olaf: model name must not have capital letters
     _name = 'oa9.zzhelper'
     _description = 'this model holds functions for the whole application that are supposed to be accessible from anywhere, for example it is used in one of the xml file that load a function'
+    # _log_access = False # Olaf: Transient Models need this.
 
-    # Olaf: We need this urgently to make the method accessible from the xml, for example
+    zzinfo = fields.Char(string="Info", default=">>> ...")
+
+    # Olaf: we cannot call a function decorated by @api.model from a button, so we need to have this workaround instead:
+    def button_flag4randomInstructors(self):
+        self.flag4randomInstructors()
+        # it needs to return something
+        return True
+
+
+    # Olaf: We need this @api.model urgently to make the method accessible from the xml, for example
+    # Olaf: we can make the method private with a leading _ but then it cannot be called anymore from the button.
     @api.model
-    def _flag4randomInstructors(self):        
+    def flag4randomInstructors(self):               
             # get 4 random partner and flag them as instructor
             p = self.env['res.partner']
             pp = p.search([])
             ppIDs = pp.ids # used to prevent double results in random
             ppSet = set()
-            for i in range(1,4):
+            for i in range(4):
                 r = randint(1,len(ppIDs))
                 ppSet.add(ppIDs[r])
                 del ppIDs[r]
             for s in ppSet:
                 pp.browse(s).instructor = True
+            self.zzinfo = str(ppSet)
 
